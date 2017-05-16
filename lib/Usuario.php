eb6fdf4d2aa5ec12122943329a6fbf17b6687d77
cetecom <cetecom@LC08L5729>
2017-05-16 11:16:19 -0400

    MD5 para todos

---
+++ lib/Usuario.php
@@ -0,0 +1,39 @@
+<?php
+
+Class Usuario{
+    
+    var $idusuario;
+    var $nombre;
+    var $clave;
+
+
+    function VerificaUsuario(){
+        $oConn= new Conexion();
+        
+        
+        if($oConn->conectar())
+            $db=$oConn->objcon;
+        else
+            return false;
+        
+        $clavemd5=  md5($this->clave);
+        $sql="SELECT * FROM acceso WHERE nomusuario='$this->nombre' and pwdusuario='$clavemd5'";
+        
+        echo $sql;
+        $resultado=$db->query($sql);
+        
+        if($resultado->num_rows>=1|| $resultado)
+            return TRUE;
+        else
+            return FALSE;        
+        
+        
+    }
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
