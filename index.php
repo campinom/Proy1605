f2952d724d1320776d0fa6c31a40bb5c1635f4d8
cetecom <cetecom@LC08L1230>
2017-05-18 12:42:16 -0400

    Hay que compartir Hay que compartir Hay que compartir (8)

--- index.php
+++ index.php
@@ -1,20 +1,35 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+session_start();
+$_SESSION["saludo"]="Hola Mundo";
+?>
+
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <a href="revision.php"> Revisi&oacute;n sesi&oacute;n</a>
+        <?php if (isset($_SESSION['USR']));{ ?>
         
-        
-        <?php
-        // put your code here
-        //primer stemen
+            <form method="post" action="revision.php">
+            <div><label>Usuario</label><input type="text" name="nombre">
+            <div><label>Clave</label><input type="password" name="clave"></br> 
+               <input type="submit" name="Acceder">
+        </form>
+        </br>
+        <?php } ?>
+        <?php 
+            echo md5('#holamundo&');
+            echo '<br>';
+            echo md5('holamundo');
+            echo '<br>';
+            echo md5('HoLaMundo');
+            echo '<br>';
+            echo md5('HOLAMundo');
+            echo '<br>';                                                                                                                                                                                    
         ?>
     </body>
+    
 </html>
--- revision.php
+++ revision.php
@@ -1,14 +1,24 @@
 <?php
+session_start();
+$_SESSION["saludo"]="Hola Mundo";
+?>
+<?php
 $PATH='lib/';
 include $PATH.'Conexion.php';
 include $PATH.'Usuario.php';
 $oUsr=new Usuario();
-$oUsr->nombre='flopez';
-$oUsr->clave='1234';
+$oUsr->nombre=$_POST['nombre'];
+$oUsr->clave=$_POST['clave'];
+
 if($oUsr->VerificaUsuarioClave())
-   echo "Existe";
-else
-    echo "No existe";
+   $_SESSION['USR']=$oUsr;
+
+
+
+header('Location:http://localhost:8081/proy1605/')
 /*session_start();
 var_dump($_SESSION);*/
+
+
+
 ?>
